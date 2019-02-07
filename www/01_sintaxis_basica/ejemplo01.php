c94b4b89cf6f31a08e464c6ac9f71db6f4f73b21
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-02-07 16:54:19 +0100

    Primeros ejemplos

---
+++ www/01_sintaxis_basica/ejemplo01.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Ejemplo 1</title>
+    <link rel="stylesheet" href="css/normalize.css"/>
+    <link rel="stylesheet" href="css/estilos.css"/>
+</head>
+<body>
+
+<?php
+
+// sintaxis_basica/ejemplo01.php
+
+echo "This is a test"; // This is one-line c++ comment
+
+/* This is a multi line comment
+   yet another line of comment */
+
+echo "This is yet another test";
+echo "One Final Test"; # This is shell-style comment
+
+?>
+
+</body>
+</html>
